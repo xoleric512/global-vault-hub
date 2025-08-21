e6c6302c8639c27c791e97158ed2902df60b3d16
xoleric512 <XolEricsson@gmail.com>
2025-08-21 23:30:31 +0300

    Update login.js
    Signed-off-by: xoleric512 <XolEricsson@gmail.com>

--- Pages/Pages/pages/pages/loginn.js
+++ Pages/Pages/pages/pages/loginn.js
@@ -1,1 +1,14 @@
+import '../styles/globals.css'
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from 'react-toastify';
 
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Component {...pageProps} />
+      <ToastContainer position="top-right" autoClose={3000}/>
+    </>
+  )
+}
+
+export default MyApp;
